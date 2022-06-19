66604a5381c84ebed6799135a6da4d6ed8b4abaa
Rui Lopes <rgl@ruilopes.com>
2022-06-19 12:16:24 +0100

    install talosctl 1.0.6

---
+++ roles/talosctl/files/install.sh
@@ -0,0 +1,18 @@
+set -euo pipefail
+
+# bail when already installed.
+if [ -x /usr/local/bin/talosctl ]; then
+    # e.g. Client v1.0.6
+    actual_version="$(/usr/local/bin/talosctl version --client --short | perl -ne '/^Client v(.+)/ && print $1')"
+    if [ "$actual_version" == "$TALOSCTL_VERSION" ]; then
+        echo 'ANSIBLE CHANGED NO'
+        exit 0
+    fi
+fi
+
+# download and install.
+talosctl_url="https://github.com/siderolabs/talos/releases/download/v${TALOSCTL_VERSION}/talosctl-linux-amd64"
+t="$(mktemp -q --suffix=.talosctl)"
+wget -q -O "$t" "$talosctl_url"
+install -m 755 "$t" /usr/local/bin/talosctl
+rm "$t"
