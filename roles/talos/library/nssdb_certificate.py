81363097f3fb30ffcfc6f6d749e3f7e007b2a4da
Rui Lopes <rgl@ruilopes.com>
2022-06-21 21:49:17 +0100

    install cert-manager

---
+++ roles/talos/library/nssdb_certificate.py
@@ -0,0 +1,131 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+DOCUMENTATION = '''
+---
+module: nssdb_certificate
+short_description: Manage the NSS certificate database
+description:
+  - Manage the NSS certificate database in ~/.pki/nssdb and in ~/snap/firefox/common/.mozilla/firefox/* (iif exists).
+options:
+  name:
+    description:
+      - Name of the certificate.
+    type: str
+  remote_src:
+    description:
+      - Path to the PEM encoded certificate to import into the nssdb.
+    type: path
+  trust:
+    description:
+      - Trust to apply to the certificate.
+      - See trustargs at https://firefox-source-docs.mozilla.org/security/nss/legacy/tools/nss_tools_certutil/index.html
+    type: str
+notes:
+  - This requires the certutil binary installed in the target host. This is normally provided by the libnss3-tools package.
+  - See https://developer.mozilla.org/en-US/docs/Mozilla/Projects/NSS/Tools
+  - See https://github.com/rgl/proxmox-ve-cluster-vagrant/blob/8e1530f74c716bf98b162d0fbca4872cb7d72d13/README.md?plain=1#L37-L55
+author:
+  - Rui Lopes (ruilopes.com)
+'''
+
+EXAMPLES = '''
+- name: Trust the Kubernetes Ingress CA
+  nssdb_certificate:
+    name: talos Kubernetes Ingress CA
+    remote_src: ~/.kube/talos-ingress-ca-crt.pem
+    trust: C,,
+'''
+
+RETURN = '''
+'''
+
+
+from ansible.module_utils.basic import AnsibleModule
+import glob
+import os
+import os.path
+import subprocess
+
+
+class NssdbCertificate(AnsibleModule):
+  def __init__(self):
+    super(NssdbCertificate, self).__init__(
+      argument_spec=dict(
+        name=dict(type='str', required=True),
+        remote_src=dict(type='path', required=True),
+        trust=dict(type='str', required=True)),
+      supports_check_mode=False)
+
+  def main(self):
+    name = self.params['name']
+    remote_src = self.params['remote_src']
+    trust = self.params['trust']
+    changed = self._trust(name, remote_src, trust)
+    self.exit_json(changed=changed, content=dict())
+
+  def _trust(self, name, remote_src, trust):
+    # see https://github.com/rgl/proxmox-ve-cluster-vagrant/blob/8e1530f74c716bf98b162d0fbca4872cb7d72d13/README.md?plain=1#L37-L55
+    remote_src = os.path.expanduser(remote_src)
+    # handle the default nssdb.
+    nssdb_path = os.path.expanduser('~/.pki/nssdb')
+    changed = self._trust_certificate(nssdb_path, name, remote_src, trust)
+    # handle the firefox snap nssdb.
+    # see https://bugs.launchpad.net/ubuntu/+source/chromium-browser/+bug/1859643
+    for p in glob.glob(os.path.expanduser('~/snap/firefox/common/.mozilla/firefox/*/cert9.db')):
+      nssdb_path = os.path.dirname(p)
+      if self._trust_certificate(nssdb_path, name, remote_src, trust):
+        changed = True
+    return changed
+
+  def _trust_certificate(self, nssdb_path, name, remote_src, trust):
+    # return False when it already exists.
+    try:
+      result = self._run('certutil', '-L', '-d', f'sql:{nssdb_path}')
+      for line in result.stdout.splitlines():
+        parts = line.strip().rsplit(' ', maxsplit=1)
+        if len(parts) != 2:
+          continue
+        certificate_name = parts[0].strip()
+        certificate_trust = parts[1].strip()
+        if certificate_name == name and certificate_trust == trust:
+          result = self._run(
+            'certutil',
+            '-L',
+            '-d', f'sql:{nssdb_path}',
+            '-n', name,
+            '-a')
+          actual = result.stdout.strip()
+          with open(remote_src) as f:
+            expected = f.read().strip()
+          if actual == expected:
+            return False
+    except:
+      pass
+    # add the certificate.
+    if not os.path.exists(nssdb_path):
+      os.makedirs(nssdb_path, mode=0o700)
+    result = self._run(
+      'certutil',
+      '-A',
+      '-d', f'sql:{nssdb_path}',
+      '-n', name,
+      '-i', remote_src,
+      '-t', trust)
+    return True
+
+  def _run(self, *args):
+    # TODO with check=True and when there is a non-zero exit code, the raised
+    #      subprocess.CalledProcessError exception does not include
+    #      stdout/stderr, which makes this impossible to troubleshoot, so we
+    #      need to include that in the exception/log.
+    return subprocess.run(args, check=True, text=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+
+def main():
+    module = NssdbCertificate()
+    module.main()
+
+
+if __name__ == '__main__':
+    main()
