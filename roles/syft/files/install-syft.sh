c8262181546eafae993aa23b980aca8aa6bb749f
Rui Lopes <rgl@ruilopes.com>
2022-06-23 21:28:32 +0100

    install syft 0.48.1

---
+++ roles/syft/files/install-syft.sh
@@ -0,0 +1,18 @@
+set -euo pipefail
+
+# bail when already installed.
+if [ -x /usr/local/bin/syft ]; then
+    # e.g. syft 0.48.1
+    actual_version="$(/usr/local/bin/syft --version | perl -ne '/^syft (.+)$/ && print $1')"
+    if [ "$actual_version" == "$SYFT_VERSION" ]; then
+        echo 'ANSIBLE CHANGED NO'
+        exit 0
+    fi
+fi
+
+# download and install.
+syft_url="https://github.com/anchore/syft/releases/download/v${SYFT_VERSION}/syft_${SYFT_VERSION}_linux_amd64.tar.gz"
+t="$(mktemp -q -d --suffix=.syft)"
+wget -qO- "$syft_url" | tar xzf - -C "$t" syft
+install -m 755 "$t/syft" /usr/local/bin/
+rm -rf "$t"
