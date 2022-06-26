3fd28d74ee11778599351b9d09720508311899d3
Rui Lopes <rgl@ruilopes.com>
2022-06-26 18:53:15 +0100

    show information about the ansible execution environment

--- ansible.sh
+++ ansible.sh
@@ -7,5 +7,15 @@ tag="ansible-$(basename "$PWD")"
 # build the ansible image.
 DOCKER_BUILDKIT=1 docker build -f Dockerfile.ansible -t "$tag" .
 
+# show information about the execution environment.
+docker run --rm -i "$tag" bash <<'EOF'
+exec 2>&1
+set -euxo pipefail
+cat /etc/os-release
+ansible --version
+python3 -m pip list
+ansible-galaxy collection list
+EOF
+
 # execute command (e.g. ansible-playbook).
 exec docker run --rm --net=host -v "$PWD:/playbooks:ro" "$tag" "$command" "$@"
