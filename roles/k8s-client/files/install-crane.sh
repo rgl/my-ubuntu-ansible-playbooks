41de7ce294c23cad6bd24d4e84e824d35a79c2d7
Rui Lopes <rgl@ruilopes.com>
2022-08-16 19:29:03 +0100

    install crane 0.11.0

---
+++ roles/k8s-client/files/install-crane.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -euo pipefail
+
+# see https://github.com/google/go-containerregistry/releases
+version="$CRANE_VERSION"
+
+# bail when already installed.
+if [ -x "/usr/local/bin/crane" ]; then
+  # e.g. 0.11.0
+  actual_version="$(/usr/local/bin/crane version)"
+  if [ "$actual_version" == "$version" ]; then
+    echo 'ANSIBLE CHANGED NO'
+    exit 0
+  fi
+fi
+
+# download and install.
+url="https://github.com/google/go-containerregistry/releases/download/v$version/go-containerregistry_Linux_x86_64.tar.gz"
+t="$(mktemp -q -d --suffix=.crane)"
+wget -qO- "$url" | tar xzf - -C "$t" --strip-components=0 crane
+install -m 755 "$t/crane" /usr/local/bin/crane
+rm -rf "$t"
