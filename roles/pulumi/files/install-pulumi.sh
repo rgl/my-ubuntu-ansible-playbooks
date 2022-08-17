5985438a448b205f91bddbb026913f69e2c7d4f8
Rui Lopes <rgl@ruilopes.com>
2022-08-17 09:24:25 +0100

    install pulumi 3.38.0

---
+++ roles/pulumi/files/install-pulumi.sh
@@ -0,0 +1,16 @@
+set -euo pipefail
+
+# bail when already installed.
+if [ -x /opt/pulumi/pulumi ]; then
+    # e.g. v3.38.0
+    actual_version="$(/opt/pulumi/pulumi version 2>/dev/null | perl -ne '/^v(.+)/ && print $1')"
+    if [ "$actual_version" == "$PULUMI_VERSION" ]; then
+        echo 'ANSIBLE CHANGED NO'
+        exit 0
+    fi
+fi
+
+# download and install.
+rm -rf /opt/pulumi
+wget -qO- "https://get.pulumi.com/releases/sdk/pulumi-v${PULUMI_VERSION}-linux-x64.tar.gz" \
+    | tar xz -C /opt
