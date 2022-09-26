090abba1548f51e60863246e1e38dd7f07b8d8e1
Rui Lopes <rgl@ruilopes.com>
2022-09-26 11:58:00 +0100

    install docker registry 2.8.1

--- roles/kind/library/kind.py
+++ roles/kind/library/kind.py
@@ -78,7 +78,18 @@ class Kind(AnsibleModule):
       changed = self._start_cluster(containers[0])
     else:
       changed = self._create_cluster()
-    network_cidr = self.docker_client.networks.get(name).attrs['IPAM']['Config'][0]['Subnet']
+    network = self.docker_client.networks.get(name)
+    network_cidr = network.attrs['IPAM']['Config'][0]['Subnet']
+    # connect the registry container to the kind network.
+    # NB this is equivalent to: docker network connect kind registry
+    registry_connected_to_network = False
+    for container in network.containers:
+      if container.name == 'registry':
+        registry_connected_to_network = True
+        break
+    if not registry_connected_to_network:
+      network.connect('registry')
+      changed = True
     self.exit_json(changed=changed, network_cidr=network_cidr)
 
   def _start_cluster(self, container):
@@ -93,12 +104,22 @@ class Kind(AnsibleModule):
     node_image_version = self.params['node_image_version']
     # create the cluster configuration.
     # see https://kind.sigs.k8s.io/docs/user/configuration/
+    # see https://kind.sigs.k8s.io/docs/user/local-registry/
+    # try wget -qSO- http://localhost:5000/v2/
+    # try wget -qSO- http://localhost:5000/v2/_catalog
+    # try docker exec -it kind-control-plane cat /etc/containerd/config.toml
+    # try docker exec -it kind-control-plane curl -v http://registry:5000/v2/
+    # try docker exec -it kind-control-plane curl -v http://registry:5000/v2/_catalog
     config = textwrap.dedent(f'''\
     apiVersion: kind.x-k8s.io/v1alpha4
     kind: Cluster
     nodes:
       - role: control-plane
         image: kindest/node:v{node_image_version}
+    containerdConfigPatches:
+      - |
+        [plugins."io.containerd.grpc.v1.cri".registry.mirrors."localhost:5000"]
+          endpoint = ["http://registry:5000"]
     ''')
     # create the cluster.
     args = [
@@ -113,20 +134,34 @@ class Kind(AnsibleModule):
     #      need to include that in the exception/log.
     subprocess.run(args, check=True, text=True, input=config, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     self._wait_for_kubernetes(25*60)
+    # document the local registry.
+    # see https://github.com/kubernetes/enhancements/tree/master/keps/sig-cluster-lifecycle/generic/1755-communicating-a-local-registry
+    kubernetes_client = kubernetes.client.CoreV1Api(self._create_kubernetes_api_client())
+    kubernetes_client.create_namespaced_config_map(
+      namespace='kube-public',
+      body=kubernetes.client.V1ConfigMap(
+        api_version='v1',
+        kind='ConfigMap',
+        metadata=dict(name='local-registry-hosting'),
+        data={
+          'localRegistryHosting.v1': textwrap.dedent('''\
+            host: "localhost:5000"
+            help: "https://kind.sigs.k8s.io/docs/user/local-registry/"
+            ''')}))
     return True
 
-  def _create_kubernetes_client(self):
+  def _create_kubernetes_api_client(self):
     context_name = f"{self.params['name']}-kind"
     contexts, active_context = kubernetes.config.list_kube_config_contexts()
     if not contexts:
       raise Exception('cannot find any cluster in kubeconfig')
     for context in contexts:
       if context['name'] == context_name:
-        return kubernetes.client.CoreApi(api_client=kubernetes.config.new_client_from_config(context=context_name))
+        return kubernetes.config.new_client_from_config(context=context_name)
     raise Exception('cannot find the cluster in kubeconfig')
 
   def _wait_for_kubernetes(self, timeout):
-    kubernetes_client = self._create_kubernetes_client()
+    kubernetes_client = kubernetes.client.CoreApi(api_client=self._create_kubernetes_api_client())
     start = time.monotonic()
     while True:
       if time.monotonic() - start > timeout:
