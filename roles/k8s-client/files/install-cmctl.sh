458db891114618057b9853cd166bfc28db3c4e40
Rui Lopes <rgl@ruilopes.com>
2022-10-08 10:17:49 +0100

    install cmctl

---
+++ roles/k8s-client/files/install-cmctl.sh
@@ -0,0 +1,18 @@
+set -euo pipefail
+
+# bail when already installed.
+if [ -x /usr/local/bin/cmctl ]; then
+    # e.g. Client Version: v1.9.1
+    actual_version="$(/usr/local/bin/cmctl version --client --short | perl -ne '/^Client Version: v(.+)/ && print $1')"
+    if [ "$actual_version" == "$CMCTL_VERSION" ]; then
+        echo 'ANSIBLE CHANGED NO'
+        exit 0
+    fi
+fi
+
+# download and install.
+cmctl_url="https://github.com/cert-manager/cert-manager/releases/download/v${CMCTL_VERSION}/cmctl-linux-amd64.tar.gz"
+t="$(mktemp -q -d --suffix=.cmctl)"
+wget -qO- "$cmctl_url" | tar xzf - -C "$t" cmctl
+install "$t/cmctl" /usr/local/bin/
+rm -rf "$t"
