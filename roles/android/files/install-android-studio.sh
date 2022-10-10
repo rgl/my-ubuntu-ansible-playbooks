df3fe4e04423fbf9571634f53d40e24696e7c81f
Rui Lopes <rgl@ruilopes.com>
2022-10-10 08:41:06 +0100

    install android studio

---
+++ roles/android/files/install-android-studio.sh
@@ -0,0 +1,20 @@
+set -euo pipefail
+
+ANDROID_STUDIO_PATH='/opt/android-studio'
+ANDROID_STUDIO_COOKIE_PATH="$ANDROID_STUDIO_PATH/.ansible-android-studio.cookie"
+
+# bail when already installed.
+if [ -f "$ANDROID_STUDIO_COOKIE_PATH" ]; then
+    actual_version="$(cat "$ANDROID_STUDIO_COOKIE_PATH")"
+    if [ "$actual_version" == "$ANDROID_STUDIO_VERSION" ]; then
+        echo 'ANSIBLE CHANGED NO'
+        exit 0
+    fi
+fi
+
+# download and install.
+url="https://redirector.gvt1.com/edgedl/android/studio/ide-zips/$ANDROID_STUDIO_VERSION/android-studio-$ANDROID_STUDIO_VERSION-linux.tar.gz"
+rm -rf "$ANDROID_STUDIO_PATH"
+install -d "$ANDROID_STUDIO_PATH"
+wget -qO- "$url" | tar xzf - -C "$ANDROID_STUDIO_PATH" --strip-components 1
+echo -n "$ANDROID_STUDIO_VERSION" >"$ANDROID_STUDIO_COOKIE_PATH"
