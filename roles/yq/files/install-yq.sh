7bb726937fac7968ab9912b7276e67b56b11cd43
Rui Lopes <rgl@ruilopes.com>
2022-10-19 08:37:55 +0100

    upgrade to yq 4.28.2

--- roles/yq/files/install-yq.sh
+++ roles/yq/files/install-yq.sh
@@ -2,7 +2,7 @@ set -euo pipefail
 
 # bail when already installed.
 if [ -x /usr/local/bin/yq ]; then
-    # e.g. yq (https://github.com/mikefarah/yq/) version 4.28.1
+    # e.g. yq (https://github.com/mikefarah/yq/) version 4.28.2
     actual_version="$(/usr/local/bin/yq --version | perl -ne '/version (.+)$/ && print $1')"
     if [ "$actual_version" == "$YQ_VERSION" ]; then
         echo 'ANSIBLE CHANGED NO'
