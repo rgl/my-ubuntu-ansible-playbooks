9445ab7ab14a8ccafc3988c44c1a2ab2a9bc121e
Rui Lopes <rgl@ruilopes.com>
2022-11-01 12:05:30 +0000

    install wasmer 2.3.0

---
+++ roles/wasmer/files/install-wasmer.sh
@@ -0,0 +1,20 @@
+set -euo pipefail
+
+wasmer="$HOME/.wasmer/bin/wasmer"
+changed='0'
+
+# install wasmer.
+# see https://github.com/wasmerio/wasmer-install
+# see https://github.com/wasmerio/wasmer-install/blob/master/install.sh
+# e.g. wasmer 2.3.0
+actual_version="$("$wasmer" --version 2>/dev/null | perl -ne '/^wasmer (.+)$/ && print $1' || true)"
+if [ "$actual_version" != "$WASMER_VERSION" ]; then
+    rm -rf ~/.wasmer
+    wget -qO- https://get.wasmer.io \
+        | sh -s "$WASMER_VERSION"
+    changed='1'
+fi
+
+if [ "$changed" == '0' ]; then
+    echo 'ANSIBLE CHANGED NO'
+fi
