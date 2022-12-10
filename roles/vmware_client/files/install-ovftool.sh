9f56c9d10ba79c3b13ee348c55aa1d832a026ea4
Rui Lopes <rgl@ruilopes.com>
2022-12-10 12:46:23 +0000

    upgrade to ovftool 4.5.0

---
+++ roles/vmware_client/files/install-ovftool.sh
@@ -0,0 +1,23 @@
+set -euxo pipefail
+
+# bail when already installed.
+if [ -f /usr/local/bin/ovftool ]; then
+    # e.g. VMware ovftool 4.5.0 (build-20459872)
+    actual_version="$(/usr/local/bin/ovftool --version | perl -ne '/^VMware ovftool (.+) \(build(-.+)\)/ && print "$1$2"')"
+    if [ "$actual_version" == "$OVFTOOL_VERSION" ]; then
+        echo 'ANSIBLE CHANGED NO'
+        exit 0
+    fi
+fi
+
+# download.
+url="https://github.com/rgl/ovftool-binaries/raw/main/archive/VMware-ovftool-$OVFTOOL_VERSION-lin.x86_64.zip"
+rm -f /tmp/ovftool.zip
+wget -q -O /tmp/ovftool.zip $url
+
+# install.
+rm -rf /opt/ovftool
+unzip -q -d /opt /tmp/ovftool.zip
+rm /tmp/ovftool.zip
+ln -fs /etc/ssl/certs/ca-certificates.crt /opt/ovftool/certs/cacert.pem
+ln -fs /opt/ovftool/ovftool /usr/local/bin/
