7005a33595d754367c65e4be84b312fff41dae63
Rui Lopes <rgl@ruilopes.com>
2023-01-12 08:36:37 +0000

    upgrade to cert-manager 1.11.0

--- roles/k8s_client/files/install-cmctl.sh
+++ roles/k8s_client/files/install-cmctl.sh
@@ -2,7 +2,7 @@ set -euo pipefail
 
 # bail when already installed.
 if [ -x /usr/local/bin/cmctl ]; then
-    # e.g. Client Version: v1.10.2
+    # e.g. Client Version: v1.11.0
     actual_version="$(/usr/local/bin/cmctl version --client --short | perl -ne '/^Client Version: v(.+)/ && print $1')"
     if [ "$actual_version" == "$CMCTL_VERSION" ]; then
         echo 'ANSIBLE CHANGED NO'
