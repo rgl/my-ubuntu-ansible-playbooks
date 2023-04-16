9937034f31db97bafcd5ce3158360470a207d387
Rui Lopes <rgl@ruilopes.com>
2023-04-16 09:54:27 +0100

    upgrade to talosctl 1.3.7

--- roles/talosctl/files/install.sh
+++ roles/talosctl/files/install.sh
@@ -2,7 +2,7 @@ set -euo pipefail
 
 # bail when already installed.
 if [ -x /usr/local/bin/talosctl ]; then
-    # e.g. Client v1.3.6
+    # e.g. Client v1.3.7
     actual_version="$(/usr/local/bin/talosctl version --client --short | perl -ne '/^Client v(.+)/ && print $1')"
     if [ "$actual_version" == "$TALOSCTL_VERSION" ]; then
         echo 'ANSIBLE CHANGED NO'
