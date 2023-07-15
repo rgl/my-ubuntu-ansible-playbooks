be144cf21e80b55270852058422b3b26050c7c6c
Rui Lopes <rgl@ruilopes.com>
2023-07-15 07:35:57 +0100

    create the ansible container image without using a tag
    this prevents conflicts with other ansible container image instances that might already exist

--- ansible.sh
+++ ansible.sh
@@ -2,13 +2,14 @@
 set -euo pipefail
 
 command="$(basename "$0" .sh)"
-tag="ansible-$(basename "$PWD")"
 
 # build the ansible image.
-DOCKER_BUILDKIT=1 docker build -f Dockerfile.ansible -t "$tag" .
+install -d tmp
+DOCKER_BUILDKIT=1 docker build -f Dockerfile.ansible --iidfile tmp/ansible-iid.txt .
+ansible_iid="$(cat tmp/ansible-iid.txt)"
 
 # show information about the execution environment.
-docker run --rm -i "$tag" bash <<'EOF'
+docker run --rm -i "$ansible_iid" bash <<'EOF'
 exec 2>&1
 set -euxo pipefail
 cat /etc/os-release
@@ -20,14 +21,14 @@ EOF
 # execute command (e.g. ansible-playbook).
 # NB the GITHUB_ prefixed environment variables are used to trigger ansible-lint
 #    to annotate the GitHub Actions Workflow with the linting violations.
-#    see https://github.com/ansible/ansible-lint/blob/v6.17.2/src/ansiblelint/app.py#L89
-#    see https://ansible-lint.readthedocs.io/usage/#ci-cd
+#    see https://github.com/ansible/ansible-lint/blob/v6.17.2/src/ansiblelint/app.py#L87
+#    see https://ansible-lint.readthedocs.io/en/latest/usage/#ci-cd
 exec docker run \
     --rm \
     --net=host \
     -v "$PWD:/project:ro" \
     -e GITHUB_ACTIONS \
     -e GITHUB_WORKFLOW \
-    "$tag" \
+    "$ansible_iid" \
     "$command" \
     "$@"
