7a0efd9b4c091169079f08b4fa75a297d25ae70d
Rui Lopes <rgl@ruilopes.com>
2023-07-22 06:05:42 +0100

    rename code plugin to code extension

--- roles/desktop/files/install-code-extension.sh
+++ roles/desktop/files/install-code-extension.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -euxo pipefail
 
-# TODO add support for upgrading the plugin to the latest version.
+# TODO add support for upgrading the extension to the latest version.
 
-expected="$CODE_PLUGIN_NAME"
+expected="$CODE_EXTENSION_NAME"
 
 # bail when the extension is already installed.
 # NB we could also use --show-versions to include the version as @VERSION suffix.
