ebf860075362db8a396ffedf94c897b9ad832a54
Rui Lopes <rgl@ruilopes.com>
2023-08-10 08:26:56 +0100

    fix the dagger version parsing

--- roles/dagger/files/install.sh
+++ roles/dagger/files/install.sh
@@ -2,8 +2,8 @@ set -euxo pipefail
 
 # bail when already installed.
 if [ -x /usr/local/bin/dagger ]; then
-    # e.g. dagger 0.2.6 (e2c2213a) linux/amd64
-    actual_version="$(/usr/local/bin/dagger version | perl -ne '/^dagger (.+?) / && print $1')"
+    # e.g. dagger v0.8.1 linux/amd64
+    actual_version="$(/usr/local/bin/dagger version | perl -ne '/^dagger v?(.+?) / && print $1')"
     if [ "$actual_version" == "$DAGGER_VERSION" ]; then
         echo 'ANSIBLE CHANGED NO'
         exit 0
