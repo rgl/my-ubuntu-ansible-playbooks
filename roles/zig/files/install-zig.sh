5c01abfb411d78d27de7a34c1f45d86b151f333a
Rui Lopes <rgl@ruilopes.com>
2023-11-19 09:39:42 +0000

    install zig 0.11.0

---
+++ roles/zig/files/install-zig.sh
@@ -0,0 +1,18 @@
+set -euxo pipefail
+
+# bail when already installed.
+if [ -x /opt/zig/zig ]; then
+    # e.g. 0.11.0
+    actual_version="$(/opt/zig/zig version | perl -ne '/^(.+)/ && print $1')"
+    if [ "$actual_version" == "$ZIG_VERSION" ]; then
+        echo 'ANSIBLE CHANGED NO'
+        exit 0
+    fi
+fi
+
+# download and install.
+# see https://ziglang.org/download/
+rm -rf /opt/zig
+install -d /opt/zig
+wget -qO- https://ziglang.org/download/$ZIG_VERSION/zig-linux-x86_64-$ZIG_VERSION.tar.xz \
+    | tar xJ -C /opt/zig --strip-components 1
