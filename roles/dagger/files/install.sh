bd3693ff06e21f933ffa8716769dcc8ae8a1f166
Rui Lopes <rgl@ruilopes.com>
2023-12-11 08:49:59 +0000

    upgrade to dagger 0.9.4

--- roles/dagger/files/install.sh
+++ roles/dagger/files/install.sh
@@ -2,7 +2,7 @@ set -euxo pipefail
 
 # bail when already installed.
 if [ -x /usr/local/bin/dagger ]; then
-    # e.g. dagger v0.9.3 linux/amd64
+    # e.g. dagger v0.9.4 linux/amd64
     actual_version="$(/usr/local/bin/dagger version | perl -ne '/^dagger v?(.+?) / && print $1')"
     if [ "$actual_version" == "$DAGGER_VERSION" ]; then
         echo 'ANSIBLE CHANGED NO'
