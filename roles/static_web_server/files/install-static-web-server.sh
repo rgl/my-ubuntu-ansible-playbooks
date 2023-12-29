a39b6888b4084fab255e7ed8ca03c265d18248e0
Rui Lopes <rgl@ruilopes.com>
2023-12-29 06:30:14 +0000

    upgrade to static-web-server 2.24.2

--- roles/static_web_server/files/install-static-web-server.sh
+++ roles/static_web_server/files/install-static-web-server.sh
@@ -2,7 +2,7 @@ set -euxo pipefail
 
 # bail when already installed.
 if [ -x /usr/local/bin/static-web-server ]; then
-    # e.g. static-web-server 2.24.1
+    # e.g. static-web-server 2.24.2
     actual_version="$(/usr/local/bin/static-web-server --version | perl -ne '/^static-web-server (.+)/ && print $1')"
     if [ "$actual_version" == "$STATIC_WEB_SERVER_VERSION" ]; then
         echo 'ANSIBLE CHANGED NO'
