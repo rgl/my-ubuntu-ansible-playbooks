dba3e8975a859ad7370b78deab6d465fb960dc74
Rui Lopes <rgl@ruilopes.com>
2024-01-03 06:00:23 +0000

    install wasmtime 16.0.0

---
+++ roles/wasmtime/files/install-wasmtime.sh
@@ -0,0 +1,18 @@
+set -euxo pipefail
+
+# bail when already installed.
+if [ -x /usr/local/bin/wasmtime ]; then
+    # e.g. wasmtime-cli 16.0.0 (6613acd1e 2023-12-20)
+    actual_version="$(/usr/local/bin/wasmtime --version | perl -ne '/^wasmtime-cli ([^ ]+)/ && print $1')"
+    if [ "$actual_version" == "$WASMTIME_VERSION" ]; then
+        echo 'ANSIBLE CHANGED NO'
+        exit 0
+    fi
+fi
+
+# download and install.
+wasmtime_url="https://github.com/bytecodealliance/wasmtime/releases/download/v${WASMTIME_VERSION}/wasmtime-v${WASMTIME_VERSION}-x86_64-linux.tar.xz"
+t="$(mktemp -q -d --suffix=.wasmtime)"
+wget -qO- "$wasmtime_url" | tar xJ -C "$t" --strip-components 1
+install -m 755 "$t/wasmtime" /usr/local/bin/
+rm -rf "$t"
