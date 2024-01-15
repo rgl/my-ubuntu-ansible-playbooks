7a45bdcc24fa469cd6bb5a34dfc2b4379b5f3791
Rui Lopes <rgl@ruilopes.com>
2024-01-15 20:13:15 +0000

    upgrade to hubble 0.13.0

--- roles/k8s_client/files/install-hubble.sh
+++ roles/k8s_client/files/install-hubble.sh
@@ -2,7 +2,7 @@ set -euxo pipefail
 
 # bail when already installed.
 if [ -x /usr/local/bin/hubble ]; then
-    # e.g. hubble 0.12.3 compiled with go1.20.10 on linux/amd64
+    # e.g. hubble 0.13.0 compiled with go1.20.10 on linux/amd64
     actual_version="$(/usr/local/bin/hubble version | perl -ne '/^hubble:? v?(.+?) / && print $1')"
     if [ "$actual_version" == "$HUBBLE_VERSION" ]; then
         echo 'ANSIBLE CHANGED NO'
