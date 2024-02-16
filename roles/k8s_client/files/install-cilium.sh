2ff3008f3b6e7eb334116e3c243be615d85f2d1d
Rui Lopes <rgl@ruilopes.com>
2024-02-16 09:24:50 +0000

    upgrade to cilium cli 0.15.23

--- roles/k8s_client/files/install-cilium.sh
+++ roles/k8s_client/files/install-cilium.sh
@@ -3,7 +3,7 @@ set -euxo pipefail
 
 # bail when already installed.
 if [ -x /usr/local/bin/cilium ]; then
-    # e.g. cilium-cli: v0.15.22 compiled with go1.21.6 on linux/amd64
+    # e.g. cilium-cli: v0.15.23 compiled with go1.21.6 on linux/amd64
     actual_version="$(/usr/local/bin/cilium version --client | perl -ne '/^cilium-cli: v(.+?) / && print $1')"
     if [ "$actual_version" == "$CILIUM_VERSION" ]; then
         echo 'ANSIBLE CHANGED NO'
