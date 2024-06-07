ae3d52f650efe4071780ec23fe7432cd14d4ad9c
Rui Lopes <rgl@ruilopes.com>
2024-06-07 08:09:37 +0100

    upgrade to cert-manager 1.15.0 and cmctl 2.0.0

--- roles/k8s_client/files/install-cmctl.sh
+++ roles/k8s_client/files/install-cmctl.sh
@@ -12,8 +12,8 @@ if [ -x /usr/local/bin/cmctl ]; then
 fi
 
 # download and install.
-cmctl_url="https://github.com/cert-manager/cert-manager/releases/download/v${CMCTL_VERSION}/cert-manager-cmctl-linux-amd64.tar.gz"
+cmctl_url="https://github.com/cert-manager/cmctl/releases/download/v${CMCTL_VERSION}/cmctl_linux_amd64"
 t="$(mktemp -q -d --suffix=.cmctl)"
-wget -qO- "$cmctl_url" | tar xzf - -C "$t" cmctl
+wget -qO "$t/cmctl" "$cmctl_url"
 install "$t/cmctl" /usr/local/bin/
 rm -rf "$t"
