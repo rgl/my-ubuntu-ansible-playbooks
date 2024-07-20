388c084689e7d5d0562c2c0265ce6574fad1d96c
Rui Lopes <rgl@ruilopes.com>
2024-07-20 06:40:33 +0100

    upgrade to dagger 0.12.1

--- roles/dagger/files/install.sh
+++ roles/dagger/files/install.sh
@@ -3,7 +3,7 @@ set -euxo pipefail
 
 # bail when already installed.
 if [ -x /usr/local/bin/dagger ]; then
-    # e.g. dagger v0.12.0 linux/amd64
+    # e.g. dagger v0.12.1 linux/amd64
     actual_version="$(/usr/local/bin/dagger version | perl -ne '/^dagger v?(.+?) / && print $1')"
     if [ "$actual_version" == "$DAGGER_VERSION" ]; then
         echo 'ANSIBLE CHANGED NO'
