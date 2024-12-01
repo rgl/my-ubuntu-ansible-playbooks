b05f4b2e48100c67e634da5512d3a43db668c169
Rui Lopes <rgl@ruilopes.com>
2024-12-01 11:22:14 +0000

    upgrade to wasmer 5.0.2

--- roles/wasmer/files/install-wasmer.sh
+++ roles/wasmer/files/install-wasmer.sh
@@ -7,7 +7,7 @@ changed='0'
 # install wasmer.
 # see https://github.com/wasmerio/wasmer-install
 # see https://github.com/wasmerio/wasmer-install/blob/master/install.sh
-# e.g. wasmer 5.0.1
+# e.g. wasmer 5.0.2
 actual_version="$("$wasmer" --version 2>/dev/null | perl -ne '/^wasmer (.+)$/ && print $1' || true)"
 if [ "$actual_version" != "$WASMER_VERSION" ]; then
     rm -rf ~/.wasmer
