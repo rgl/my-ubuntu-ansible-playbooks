ccfbbc775abbfa3566374198838dabc1368d88b9
Rui Lopes <rgl@ruilopes.com>
2024-12-20 18:38:09 +0000

    upgrade to terramate 0.11.5

--- roles/terramate/files/install-terramate.sh
+++ roles/terramate/files/install-terramate.sh
@@ -3,7 +3,7 @@ set -euxo pipefail
 
 # bail when already installed.
 if [ -x /usr/local/bin/terramate ]; then
-    # e.g. 0.11.4
+    # e.g. 0.11.5
     actual_version="$(/usr/local/bin/terramate version)"
     if [ "$actual_version" == "$TERRAMATE_VERSION" ]; then
         echo 'ANSIBLE CHANGED NO'
