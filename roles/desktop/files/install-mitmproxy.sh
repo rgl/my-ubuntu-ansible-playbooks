0eb28f4e36b03dff973395eb92532f07980ac92e
Rui Lopes <rgl@ruilopes.com>
2025-02-14 08:28:31 +0000

    upgrade to mitmproxy 11.1.2

--- roles/desktop/files/install-mitmproxy.sh
+++ roles/desktop/files/install-mitmproxy.sh
@@ -3,7 +3,7 @@ set -euxo pipefail
 
 # bail when already installed.
 if [ -r /usr/local/bin/mitmproxy ]; then
-    # e.g. Mitmproxy: 11.1.0 binary
+    # e.g. Mitmproxy: 11.1.2 binary
     actual_version="$(/usr/local/bin/mitmproxy --version | perl -ne '/^Mitmproxy: (.+?) / && print $1')"
     if [ "$actual_version" == "$MITMPROXY_VERSION" ]; then
         echo 'ANSIBLE CHANGED NO'
