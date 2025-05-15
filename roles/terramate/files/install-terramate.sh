e0dfba5a83ca68c5109480ecda54472a1ad19568
Rui Lopes <rgl@ruilopes.com>
2025-05-15 07:04:19 +0000

    upgrade to terramate 0.13.1

--- roles/terramate/files/install-terramate.sh
+++ roles/terramate/files/install-terramate.sh
@@ -3,7 +3,7 @@ set -euxo pipefail
 
 # bail when already installed.
 if [ -x /usr/local/bin/terramate ]; then
-    # e.g. 0.13.0
+    # e.g. 0.13.1
     actual_version="$(/usr/local/bin/terramate version)"
     if [ "$actual_version" == "$TERRAMATE_VERSION" ]; then
         echo 'ANSIBLE CHANGED NO'
