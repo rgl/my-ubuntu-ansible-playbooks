18775e3a146042690b0ac4ec2ea1ea330853fa82
Rui Lopes <rgl@ruilopes.com>
2025-06-14 06:26:45 +0000

    update the wasmtime version parsing

--- roles/wasmtime/files/install-wasmtime.sh
+++ roles/wasmtime/files/install-wasmtime.sh
@@ -3,8 +3,8 @@ set -euxo pipefail
 
 # bail when already installed.
 if [ -x /usr/local/bin/wasmtime ]; then
-    # e.g. wasmtime-cli 16.0.0 (6613acd1e 2023-12-20)
-    actual_version="$(/usr/local/bin/wasmtime --version | perl -ne '/^wasmtime-cli ([^ ]+)/ && print $1')"
+    # e.g. wasmtime 33.0.0 (4ce8232ab 2025-05-20)
+    actual_version="$(/usr/local/bin/wasmtime --version | perl -ne '/^wasmtime ([^ ]+)/ && print $1')"
     if [ "$actual_version" == "$WASMTIME_VERSION" ]; then
         echo 'ANSIBLE CHANGED NO'
         exit 0
