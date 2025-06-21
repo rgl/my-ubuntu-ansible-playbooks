7cae8acd3d37591b5d6e00f7a17baff02d1409d3
Rui Lopes <rgl@ruilopes.com>
2025-06-21 06:12:52 +0000

    fix the code extension installation script logic

--- roles/desktop/files/install-code-extension.sh
+++ roles/desktop/files/install-code-extension.sh
@@ -8,12 +8,10 @@ expected="$CODE_EXTENSION_NAME"
 # bail when the extension is already installed.
 # NB we could also use --show-versions to include the version as @VERSION suffix.
 # NB extension ids are case-insensitive.
-code --list-extensions | while read -r actual; do
-    if [ "${actual,,}" == "${expected,,}" ]; then
-        echo 'ANSIBLE CHANGED NO'
-        exit 0
-    fi
-done
+if code --list-extensions | grep -q -i -F -x "$expected"; then
+    echo 'ANSIBLE CHANGED NO'
+    exit 0
+fi
 
 # install.
 code --install-extension "$expected"
