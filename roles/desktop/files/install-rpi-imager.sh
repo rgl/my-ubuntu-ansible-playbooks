1529dbef63e94710fd5b8fcd109fbd86daeadccd
Rui Lopes <rgl@ruilopes.com>
2025-06-21 06:12:52 +0000

    upgrade to rpi-imager 1.9.4

--- roles/desktop/files/install-rpi-imager.sh
+++ roles/desktop/files/install-rpi-imager.sh
@@ -2,13 +2,8 @@
 set -euxo pipefail
 
 # bail when already installed.
-if [ -r /usr/local/bin/rpi-imager ]; then
-    # NB rpi-imager.AppImage --version does not work from an ssh session, so
-    #    we have to work around that, by extracting the
-    #    org.raspberrypi.rpi-imager.desktop file from the AppImage, then the
-    #    X-AppImage-Version property value.
-    # e.g. X-AppImage-Version=1.9.0
-    actual_version="$(7z x -so /usr/local/bin/rpi-imager '*.desktop' | perl -ne '/^X-AppImage-Version=(.+)/ && print $1')"
+if [ -r /usr/local/share/applications/rpi-imager.desktop ]; then
+    actual_version="$(perl -ne '/^# RPI_IMAGER_VERSION: (.+)/ && print $1' /usr/local/share/applications/rpi-imager.desktop)"
     if [ "$actual_version" == "$RPI_IMAGER_VERSION" ]; then
         echo 'ANSIBLE CHANGED NO'
         exit 0
@@ -18,12 +13,16 @@ fi
 # download and install.
 url="https://github.com/raspberrypi/rpi-imager/releases/download/v${RPI_IMAGER_VERSION}/Raspberry_Pi_Imager-${RPI_IMAGER_VERSION}-x86_64.AppImage"
 t="$(mktemp -q -d --suffix=.rpi-imager)"
-wget -qO "$t/rpi-imager" "$url"
-7z x "$t/rpi-imager" "-o$t" usr/share/icons/hicolor/128x128/apps/rpi-imager.png
-cat >/usr/local/share/applications/rpi-imager.desktop <<'EOF'
+pushd "$t"
+wget -qO rpi-imager "$url"
+chmod +x rpi-imager
+# NB use rpi-imager --appimage-help to known what AppImage options are available.
+./rpi-imager --appimage-extract usr/share/icons/hicolor/128x128/apps/rpi-imager.png
+cat >/usr/local/share/applications/rpi-imager.desktop <<EOF
+# RPI_IMAGER_VERSION: $RPI_IMAGER_VERSION
 [Desktop Entry]
 Type=Application
-Version=1.0
+Version=1.5
 Name=Raspberry Pi Imager
 Comment=Raspberry Pi Imager
 Icon=/usr/local/share/icons/hicolor/128x128/apps/rpi-imager.png
@@ -31,6 +30,7 @@ Exec=rpi-imager --disable-telemetry %F
 Categories=Utility
 StartupNotify=false
 EOF
-cp -r "$t/usr/"* /usr/local
-install -m 755 "$t/rpi-imager" /usr/local/bin
+cp -r squashfs-root/usr/* /usr/local
+install -m 755 rpi-imager /usr/local/bin
+popd
 rm -rf "$t"
