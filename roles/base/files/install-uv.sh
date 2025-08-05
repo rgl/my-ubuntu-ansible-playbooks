1b5a1a2786f17e8a16929c43def6ab79cdefadc2
Rui Lopes <rgl@ruilopes.com>
2025-08-05 07:26:32 +0000

    install uv 0.8.4

---
+++ roles/base/files/install-uv.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -euxo pipefail
+
+# bail when already installed.
+if [ -x /usr/local/bin/uv ]; then
+    # e.g.: uv 0.8.4
+    actual_version="$(/usr/local/bin/uv --version | perl -ne '/^uv (.+)/ && print $1')"
+    if [ "$actual_version" == "$UV_VERSION" ]; then
+        echo 'ANSIBLE CHANGED NO'
+        exit 0
+    fi
+fi
+
+# download and install.
+url="https://github.com/astral-sh/uv/releases/download/$UV_VERSION/uv-x86_64-unknown-linux-gnu.tar.gz"
+t="$(mktemp -q -d --suffix=.uv)"
+pushd "$t"
+wget -qO- "$url" | tar xzf - --strip-components 1
+install -m 755 uv uvx /usr/local/bin
+popd
+rm -rf "$t"
