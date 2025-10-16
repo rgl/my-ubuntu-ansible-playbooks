6489387179f50b338011cf2c99d5314b0ad1336b
Rui Lopes <rgl@ruilopes.com>
2025-10-16 07:02:57 +0000

    upgrade to talosctl 1.11.3

--- roles/talosctl/files/install.sh
+++ roles/talosctl/files/install.sh
@@ -3,7 +3,7 @@ set -euxo pipefail
 
 # bail when already installed.
 if [ -x /usr/local/bin/talosctl ]; then
-    # e.g. Talos v1.11.2
+    # e.g. Talos v1.11.3
     actual_version="$(/usr/local/bin/talosctl version --client --short | perl -ne '/^Talos v(.+)/ && print $1')"
     if [ "$actual_version" == "$TALOSCTL_VERSION" ]; then
         echo 'ANSIBLE CHANGED NO'
