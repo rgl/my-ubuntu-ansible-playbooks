d0d35188659ed68afa51d97bdf3767878c3cf7cc
Rui Lopes <rgl@ruilopes.com>
2025-10-20 22:45:54 +0000

    upgrade to uv 0.9.4

--- roles/base/files/install-uv.sh
+++ roles/base/files/install-uv.sh
@@ -3,7 +3,7 @@ set -euxo pipefail
 
 # bail when already installed.
 if [ -x /usr/local/bin/uv ]; then
-    # e.g.: uv 0.9.3
+    # e.g.: uv 0.9.4
     actual_version="$(/usr/local/bin/uv --version | perl -ne '/^uv (.+)/ && print $1')"
     if [ "$actual_version" == "$UV_VERSION" ]; then
         echo 'ANSIBLE CHANGED NO'
