c21bdbe5362c7e8f86d000801e458c1d55d88032
Rui Lopes <rgl@ruilopes.com>
2025-12-26 23:34:55 +0000

    upgrade to rpi-imager 2.0.3

--- roles/desktop/files/install-rpi-imager.sh
+++ roles/desktop/files/install-rpi-imager.sh
@@ -11,25 +11,27 @@ if [ -r /usr/local/share/applications/rpi-imager.desktop ]; then
 fi
 
 # download and install.
-url="https://github.com/raspberrypi/rpi-imager/releases/download/v${RPI_IMAGER_VERSION}/Raspberry_Pi_Imager-${RPI_IMAGER_VERSION}-x86_64.AppImage"
+url="https://github.com/raspberrypi/rpi-imager/releases/download/v${RPI_IMAGER_VERSION}/Raspberry_Pi_Imager-v${RPI_IMAGER_VERSION}-desktop-x86_64.AppImage"
 t="$(mktemp -q -d --suffix=.rpi-imager)"
 pushd "$t"
 wget -qO rpi-imager "$url"
 chmod +x rpi-imager
 # NB use rpi-imager --appimage-help to known what AppImage options are available.
-./rpi-imager --appimage-extract usr/share/icons/hicolor/128x128/apps/rpi-imager.png
+./rpi-imager --appimage-extract usr/share/icons/hicolor/scalable/apps/rpi-imager.svg
 cat >/usr/local/share/applications/rpi-imager.desktop <<EOF
 # RPI_IMAGER_VERSION: $RPI_IMAGER_VERSION
 [Desktop Entry]
 Type=Application
 Version=1.5
 Name=Raspberry Pi Imager
-Comment=Raspberry Pi Imager
-Icon=/usr/local/share/icons/hicolor/128x128/apps/rpi-imager.png
-Exec=rpi-imager --disable-telemetry %F
-Categories=Utility
+Comment=Tool for writing images to SD cards for Raspberry Pi
+Icon=rpi-imager
+Exec=pkexec --disable-internal-agent /usr/local/bin/rpi-imager --disable-telemetry %u
+Categories=Utility;
 StartupNotify=false
+MimeType=x-scheme-handler/rpi-imager;application/vnd.raspberrypi.imager-manifest+json;
 EOF
+rm -f /usr/local/share/icons/hicolor/*/apps/rpi-imager.*
 cp -r squashfs-root/usr/* /usr/local
 install -m 755 rpi-imager /usr/local/bin
 popd
