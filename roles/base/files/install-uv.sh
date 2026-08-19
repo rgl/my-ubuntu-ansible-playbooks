f384c52c8cbf58409c9dd658d6c14094244e3ff7
Rui Lopes <rgl@ruilopes.com>
2026-08-19 06:00:19 +0000

    upgrade to uv 0.12.5

--- roles/base/files/install-uv.sh
+++ roles/base/files/install-uv.sh
@@ -3,7 +3,7 @@ set -euxo pipefail
 
 # bail when already installed.
 if [ -x /usr/local/bin/uv ]; then
-    # e.g.: uv 0.12.3 (x86_64-unknown-linux-gnu)
+    # e.g.: uv 0.12.5 (x86_64-unknown-linux-gnu)
     actual_version="$(/usr/local/bin/uv --version | perl -ne '/^uv ([^ ]+)/ && print $1')"
     if [ "$actual_version" == "$UV_VERSION" ]; then
         echo 'ANSIBLE CHANGED NO'
